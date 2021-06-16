939c1b6cfe993d44eac944300298fb3ca70a3040
Nimish Shah <nimishshah2000@gmail.com>
2021-06-17 02:28:59 +0530

    check dp approach

--- choose_transaction.cpp
+++ choose_transaction.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 #define pb push_back
+#define MAX_WEIGHT 400000
+
+vector<vector<int>> dp(5215, vector<int>(400001, -1));
 
 class Transaction {
 	string txn_id;
@@ -40,6 +43,15 @@ class Transaction {
 	}
 };
 
+// int sol(vector<Transaction *> t, int weight, int n) {
+// 	if(n==0 || weight==0) return 0;
+
+// 	if(t[n-1]->getWeight() <= weight) {
+// 		return max(sol(t, weight - t[n-1]->getWeight(), n-1) + t[n-1]->getFee(), sol(t, weight, n-1));
+// 	} else {
+// 		return sol(t, weight, n-1);
+// 	}
+// }
 
 int main() {
 	vector<Transaction*> transactions;
@@ -59,4 +71,37 @@ int main() {
 	}
 	fin.close();
 
+	cout<<transactions.size()<<endl;
+
+	
+	// memset(dp, -1, sizeof(dp));
+
+	// dp = new int*[transactions.size()+1];
+
+	cout<<"INIT DONE"<<endl;
+	// for(int i=0;i<transactions.size()+1;i++) {
+	// 	dp[i] = new int[MAX_WEIGHT+1];
+	// }
+
+	// cout<<"INIT DONE"<<endl;
+
+	for(int i=1;i<transactions.size()+1;i++) {
+		for(int j=1;j<MAX_WEIGHT+1;j++) {
+			if(i==0 || j==0) {
+				dp[i][j] = 0;
+			}
+			if(transactions[i-1]->getWeight() <= j) {
+				dp[i][j] = max(transactions[i-1]->getFee() + dp[i-1][j-transactions[i-1]->getWeight()], dp[i-1][j-1]);
+				// cout<<"A";
+			} else {
+				dp[i][j] = dp[i-1][j-1];
+			}
+		}
+	}
+	cout<<"DOne\n";
+
+	cout<<dp[transactions.size()][MAX_WEIGHT]<<endl;
+
+	// cout<<sol(transactions, MAX_WEIGHT, transactions.size())<<endl;
 }
+
